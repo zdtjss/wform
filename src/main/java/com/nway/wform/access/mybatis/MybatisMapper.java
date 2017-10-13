e6c33929f2395c0621099be312054d152695941c
张德涛 <zhangdetao@chinasofti.com>
2017-10-13 17:59:59 +0800

    MyBatis mapper 操作

---
+++ src/main/java/com/nway/wform/access/mybatis/MybatisMapper.java
@@ -0,0 +1,30 @@
+package com.nway.wform.access.mybatis;
+
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.session.Configuration;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.NestedIOException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MybatisMapper {
+	
+	@Autowired
+    private SqlSessionTemplate sqlSession;
+
+	public void addMapper(String mapperLocation) throws NestedIOException {
+
+		Configuration configuration = sqlSession.getConfiguration();
+		ClassPathResource resource = new ClassPathResource(mapperLocation);
+
+		try {
+			XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(resource.getInputStream(), configuration,
+					resource.toString(), configuration.getSqlFragments());
+			xmlMapperBuilder.parse();
+		} catch (Exception e) {
+			throw new NestedIOException("Failed to parse mapping resource: '" + mapperLocation + "'", e);
+		}
+	}
+}
