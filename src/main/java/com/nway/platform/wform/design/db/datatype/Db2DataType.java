362eb2249fa648264f7bd16f5a63499a3e01c7b6
张德涛 <zhangdetao@chinasofti.com>
2018-03-04 13:28:02 +0800

    no message

--- src/main/java/com/nway/platform/wform/design/db/datatype/Db2DataType.java
+++ src/main/java/com/nway/platform/wform/design/db/datatype/Db2DataType.java
@@ -1,7 +1,5 @@
 package com.nway.platform.wform.design.db.datatype;
 
-import org.springframework.stereotype.Component;
-
 public class Db2DataType extends BaseDataType {
 
 
--- src/main/java/com/nway/platform/wform/design/db/datatype/HsqlDataType.java
+++ src/main/java/com/nway/platform/wform/design/db/datatype/HsqlDataType.java
@@ -1,7 +1,5 @@
 package com.nway.platform.wform.design.db.datatype;
 
-import org.springframework.stereotype.Component;
-
 public class HsqlDataType extends BaseDataType {
 
 
